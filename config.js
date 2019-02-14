e4b77c1b11e7a7b041a269fdeb8c61d2e911663f
Michael Ajala <agmt5989@gmail.com>
2019-02-14 18:58:38 +0100

    the config file too

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/jwt',
 
   // 2. JWT
-  TOKEN_SECRET: process.env.TOKEN_SECRET || 'pvpnCCZfwOF85pBjbOebZiYIDhZ3w9LZrKwBZ7152K89mPCOHtbRlmr5Z91ci4L',
+  TOKEN_SECRET: process.env.TOKEN_SECRET || '505cb5dacd775e51e19faf0c697221a86159f09d0e410d2de7adee91adb32bc9620a62213095df3b7aac154cad55b505f261a858f5fbf8caab47b166b3716295',
 
   // 3. Express Server Port
   LISTEN_PORT: process.env.LISTEN_PORT || 3000
